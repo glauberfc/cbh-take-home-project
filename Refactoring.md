bf833b2ad277e5cc06cb891d4cba7d6a3df8df2a
Glauber Castro <glauber.castro@deel.com>
2023-06-04 16:07:52 -0300

    refactor: improve deterministicPartitionKey

--- Refactoring.md
+++ Refactoring.md
@@ -9,3 +9,4 @@ You've been asked to refactor the function `deterministicPartitionKey` in [`dpk.
 You will be graded on the exhaustiveness and quality of your unit tests, the depth of your refactor, and the level of insight into your thought process provided by the written explanation.
 
 ## Your Explanation Here
+In my refactoring I tried to break the `deterministicPartitionKey` function into small functions, because that makes clear what each block of code is doing. In addition, I removed all nested `if` blocks to clarify the rules for each check. I would give each little function derived from `deterministicPartitionKey` a better name in a real day's work, because I would ask for more information to understand the business rules behind it, but this was the best I could do with the time and knowledge limitations for this challenge.
--- dpk.js
+++ dpk.js
@@ -1,29 +1,41 @@
 const crypto = require('crypto')
 
 exports.deterministicPartitionKey = (event) => {
-  const TRIVIAL_PARTITION_KEY = '0'
   const MAX_PARTITION_KEY_LENGTH = 256
-  let candidate
 
-  if (event) {
-    if (event.partitionKey) {
-      candidate = event.partitionKey
-    } else {
-      const data = JSON.stringify(event)
-      candidate = crypto.createHash('sha3-512').update(data).digest('hex')
-    }
-  }
+  const partitionKey = getPartitionKey(event)
+  const candidate = getCandidate(partitionKey)
 
-  if (candidate) {
-    if (typeof candidate !== 'string') {
-      candidate = JSON.stringify(candidate)
-    }
-  } else {
-    candidate = TRIVIAL_PARTITION_KEY
-  }
   if (candidate.length > MAX_PARTITION_KEY_LENGTH) {
-    candidate = crypto.createHash('sha3-512').update(candidate).digest('hex')
+    return crypto.createHash('sha3-512').update(candidate).digest('hex')
   }
 
   return candidate
+}
+
+function getPartitionKey(event) {
+  if (event?.partitionKey) {
+    return event.partitionKey
+  }
+
+  if (event) {
+    const data = JSON.stringify(event)
+    return crypto.createHash('sha3-512').update(data).digest('hex')
+  }
+
+  return undefined
+}
+
+function getCandidate(candidate) {
+  const TRIVIAL_PARTITION_KEY = '0'
+
+  if (candidate && typeof candidate !== 'string') {
+    return JSON.stringify(candidate)
+  }
+
+  if (candidate && typeof candidate === 'string') {
+    return candidate
+  }
+
+  return TRIVIAL_PARTITION_KEY
 }
